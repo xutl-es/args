157a69fad18ab41eee056db9076b27b5ea9e08f3
XUTL Dev <dev@xutl.es>
2021-06-25 18:26:41 +0100

    fix extraction of mingw start args (#1)

--- lib/extract.ts
+++ lib/extract.ts
@@ -1,12 +1,15 @@
-import { resolve, basename } from 'path';
+import { resolve as resolvePath, basename, dirname, extname, join as joinPath } from 'path';
 
 const re_assignment = /^(--\w+)=(.*)$/;
 const re_flag = /^-(\w+)$/;
 
+const resolved = Array.from(new Set([resolvePath(require?.main?.filename ?? ''), resolve(require?.main?.filename ?? '')]));
+const unresolved = Array.from(new Set(resolved.map((s) => basename(s))));
+
 export function extract(argv: string[]): string[] {
 	const args = [...argv];
 	if (args[0] && (resolve(args[0]) === resolve(process.argv0) || basename(args[0]) === 'node')) args.shift();
-	if (args[0] && require.main && resolve(args[0]) === resolve(require.main.filename)) args.shift();
+	if (args[0] && (resolved.includes(resolve(args[0])) || unresolved.includes(args[0]))) args.shift();
 	const result: string[] = [];
 	let done = false;
 	for (const item of args) {
@@ -33,4 +36,12 @@ export function extract(argv: string[]): string[] {
 		result.push(item);
 	}
 	return result;
+}
+
+function resolve(path: string): string {
+	path = resolvePath(path);
+	const dir = dirname(path);
+	const ext = extname(path) || '.js';
+	const bas = basename(path, ext);
+	return joinPath(dir, `${bas}.${ext}`);
 }
